56d0cf19a53a03ff205f1b371ca005d525d0a68f
Fatma <fceyda@gmail.com>
2020-03-10 22:38:35 -0700

    deleting list items

--- ToDoList/Controllers/ItemsControllers.cs
+++ ToDoList/Controllers/ItemsControllers.cs
@@ -14,7 +14,7 @@ namespace ToDoList.Controllers
     }
 
       [HttpGet("/items/new")]
-    public ActionResult CreateForm()
+    public ActionResult New()
     {
       return View();
     }
@@ -24,6 +24,19 @@ namespace ToDoList.Controllers
     {
       Item myItem = new Item(description);
       return RedirectToAction("Index");
+    }
+
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
+     [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item foundItem = Item.Find(id);
+      return View(foundItem);
     }
   }
 }
